61e01218d3797b30bf3eb29f3c02cb5af872e281
Moritz Voelker <e228233@edu.sbl.ch>
2025-12-15 20:42:24 +0000

    Änderung 1: Ich habe zwei Buttons erstellt, mit welchen man die Farbe auf der Website anpassen kann. (Java script) Änderung 2: Farbwahl der SVG Icons über Klassen. Änderung 3: Layout Gestaltung der Unterseiten einheitlich gestaltet.

--- main.js
+++ main.js
@@ -1,12 +1,13 @@
-let counter = 0;
+function changeDunkel() {
+  document.documentElement.style.setProperty("--primary", "blue");
+  document.documentElement.style.setProperty("--secondary", "darkblue");
+  document.documentElement.style.setProperty("--background", "black");
+  document.documentElement.style.setProperty("--fontColor", "white");
+}
 
-function createNewElement() {
-  let newElement = document.createElement("p");
-
-  counter = counter + 1;
-
-  newElement.textContent = "Element " + counter;
-
-  let mainElement = document.querySelector("main");
-  mainElement.appendChild(newElement);
+function changeHell() {
+  document.documentElement.style.setProperty("--primary", "mintcream");
+  document.documentElement.style.setProperty("--secondary", "lightblue");
+  document.documentElement.style.setProperty("--background", "white");
+  document.documentElement.style.setProperty("--fontColor", "black");
 }
